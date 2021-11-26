4cac56b30952bb667e8ab2ddc3fd2930e6fd210e
vanhxinhdep <95084376+vanhxinhdep@users.noreply.github.com>
2021-11-26 20:57:27 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+https://www.facebook.com/thisisslotus243/
+> Các bạn vào page của mình nha :3
---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Tau có điều này muốn hỏi Ngưi, Ngưi nhớ phải trả lời thật lòng nhaaa`,
+    btnIntro: 'OK',
+    title: 'Tau biết Mi thích Tau cóa phải khum phải khum ? 😙',
+    desc: 'Mi không trả lời hoặc thoát ra, tức là Mi thích Tau đó nha muhahahaha :> ',
+    btnYes: 'Đúmg, tau thật sự thích mii 💖💖💖',
+    btnNo: '💔 Mi mơ à 🙂 ! 💔 ',
+    question:'Nói cho Tau biết đi vì sao Mi thích tau 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'tại vì tau iu mi á <3333',
+    mess: 'Tau biết mà, Tau cũng thích Mi lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Tau nhaaaaa. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/vuanh.nguyen.16144606'
+}
